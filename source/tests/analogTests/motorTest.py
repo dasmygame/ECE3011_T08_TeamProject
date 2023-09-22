23a18f40a834fda1bdd93825aa4cceb105b5dea5
Rahul Das <48290252+dasmygame@users.noreply.github.com>
2023-09-22 12:42:20 -0400

    Create motorTest.py

---
+++ source/tests/analogTests/motorTest.py
@@ -0,0 +1,7 @@
+def on_button_pressed_a():
+    pins.servo_write_pin(AnalogPin.P0, 180)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    pins.servo_write_pin(AnalogPin.P0, 90)
+input.on_button_pressed(Button.B, on_button_pressed_b)
